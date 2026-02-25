3a1021f2013435794cb1c8ee4eb6bf26750ee888
maltheaugust <s214631@dtu.dk>
2026-02-25 08:38:05 +0100

    added normalizing flow code from exercises

---
+++ Project1/src/vae/flow.py
@@ -0,0 +1,227 @@
+# Code for DTU course 02460 (Advanced Machine Learning Spring) by Jes Frellsen, 2024
+# Version 1.3 (2024-02-11)
+# Inspiration is taken from:
+# - https://github.com/jmtomczak/intro_dgm/blob/main/flows/realnvp_example.ipynb
+# - https://github.com/VincentStimper/normalizing-flows/tree/master
+
+import torch
+import torch.nn as nn
+import torch.distributions as td
+from tqdm import tqdm
+
+class GaussianBase(nn.Module):
+    def __init__(self, D):
+        """
+        Define a Gaussian base distribution with zero mean and unit variance.
+
+                Parameters:
+        M: [int] 
+           Dimension of the base distribution.
+        """
+        super(GaussianBase, self).__init__()
+        self.D = D
+        self.mean = nn.Parameter(torch.zeros(self.D), requires_grad=False)
+        self.std = nn.Parameter(torch.ones(self.D), requires_grad=False)
+
+    def forward(self):
+        """
+        Return the base distribution.
+
+        Returns:
+        prior: [torch.distributions.Distribution]
+        """
+        return td.Independent(td.Normal(loc=self.mean, scale=self.std), 1)
+
+class MaskedCouplingLayer(nn.Module):
+    """
+    An affine coupling layer for a normalizing flow.
+    """
+
+    def __init__(self, scale_net, translation_net, mask):
+        """
+        Define a coupling layer.
+
+        Parameters:
+        scale_net: [torch.nn.Module]
+            The scaling network that takes as input a tensor of dimension `(batch_size, feature_dim)` and outputs a tensor of dimension `(batch_size, feature_dim)`.
+        translation_net: [torch.nn.Module]
+            The translation network that takes as input a tensor of dimension `(batch_size, feature_dim)` and outputs a tensor of dimension `(batch_size, feature_dim)`.
+        mask: [torch.Tensor]
+            A binary mask of dimension `(feature_dim,)` that determines which features (where the mask is zero) are transformed by the scaling and translation networks.
+        """
+        super(MaskedCouplingLayer, self).__init__()
+        self.scale_net = scale_net
+        self.translation_net = translation_net
+        self.mask = nn.Parameter(mask, requires_grad=False)
+
+    def forward(self, z):
+        """
+        Transform a batch of data through the coupling layer (from the base to data).
+
+        Parameters:
+        x: [torch.Tensor]
+            The input to the transformation of dimension `(batch_size, feature_dim)`
+        Returns:
+        z: [torch.Tensor]
+            The output of the transformation of dimension `(batch_size, feature_dim)`
+        sum_log_det_J: [torch.Tensor]
+            The sum of the log determinants of the Jacobian matrices of the forward transformations of dimension `(batch_size, feature_dim)`.
+        """
+        z_masked = z * self.mask 
+        
+        s = self.scale_net(z_masked) * (1 - self.mask)
+        t = self.translation_net(z_masked) * (1 - self.mask)
+        
+        x = z_masked + (z * (1 - self.mask)) * torch.exp(s) + t
+        
+        log_det_J = s.sum(dim=1)
+        return x, log_det_J
+    
+    def inverse(self, x):
+        """
+        Transform a batch of data through the coupling layer (from data to the base).
+
+        Parameters:
+        z: [torch.Tensor]
+            The input to the inverse transformation of dimension `(batch_size, feature_dim)`
+        Returns:
+        x: [torch.Tensor]
+            The output of the inverse transformation of dimension `(batch_size, feature_dim)`
+        sum_log_det_J: [torch.Tensor]
+            The sum of the log determinants of the Jacobian matrices of the inverse transformations.
+        """
+        z = x
+        log_det_J = torch.zeros(x.shape[0])
+        return z, log_det_J
+
+
+class Flow(nn.Module):
+    def __init__(self, base, transformations):
+        """
+        Define a normalizing flow model.
+        
+        Parameters:
+        base: [torch.distributions.Distribution]
+            The base distribution.
+        transformations: [list of torch.nn.Module]
+            A list of transformations to apply to the base distribution.
+        """
+        super(Flow, self).__init__()
+        self.base = base
+        self.transformations = nn.ModuleList(transformations)
+
+    def forward(self, z):
+        """
+        Transform a batch of data through the flow (from the base to data).
+        
+        Parameters:
+        x: [torch.Tensor]
+            The input to the transformation of dimension `(batch_size, feature_dim)`
+        Returns:
+        z: [torch.Tensor]
+            The output of the transformation of dimension `(batch_size, feature_dim)`
+        sum_log_det_J: [torch.Tensor]
+            The sum of the log determinants of the Jacobian matrices of the forward transformations.            
+        """
+        sum_log_det_J = 0
+        for T in self.transformations:
+            x, log_det_J = T(z)
+            sum_log_det_J += log_det_J
+            z = x
+        return x, sum_log_det_J
+    
+    def inverse(self, x):
+        """
+        Transform a batch of data through the flow (from data to the base).
+
+        Parameters:
+        x: [torch.Tensor]
+            The input to the inverse transformation of dimension `(batch_size, feature_dim)`
+        Returns:
+        z: [torch.Tensor]
+            The output of the inverse transformation of dimension `(batch_size, feature_dim)`
+        sum_log_det_J: [torch.Tensor]
+            The sum of the log determinants of the Jacobian matrices of the inverse transformations.
+        """
+        sum_log_det_J = 0
+        for T in reversed(self.transformations):
+            z, log_det_J = T.inverse(x)
+            sum_log_det_J += log_det_J
+            x = z
+        return z, sum_log_det_J
+    
+    def log_prob(self, x):
+        """
+        Compute the log probability of a batch of data under the flow.
+
+        Parameters:
+        x: [torch.Tensor]
+            The data of dimension `(batch_size, feature_dim)`
+        Returns:
+        log_prob: [torch.Tensor]
+            The log probability of the data under the flow.
+        """
+        z, log_det_J = self.inverse(x)
+        return self.base().log_prob(z) + log_det_J
+    
+    def sample(self, sample_shape=(1,)):
+        """
+        Sample from the flow.
+
+        Parameters:
+        n_samples: [int]
+            Number of samples to generate.
+        Returns:
+        z: [torch.Tensor]
+            The samples of dimension `(n_samples, feature_dim)`
+        """
+        z = self.base().sample(sample_shape)
+        return self.forward(z)[0]
+    
+    def loss(self, x):
+        """
+        Compute the negative mean log likelihood for the given data bath.
+
+        Parameters:
+        x: [torch.Tensor] 
+            A tensor of dimension `(batch_size, feature_dim)`
+        Returns:
+        loss: [torch.Tensor]
+            The negative mean log likelihood for the given data batch.
+        """
+        return -torch.mean(self.log_prob(x))
+
+
+def train(model, optimizer, data_loader, epochs, device):
+    """
+    Train a Flow model.
+
+    Parameters:
+    model: [Flow]
+       The Flow model to train.
+    optimizer: [torch.optim.Optimizer]
+         The optimizer to use for training.
+    data_loader: [torch.utils.data.DataLoader]
+            The data loader to use for training.
+    epochs: [int]
+        Number of epochs to train for.
+    device: [torch.device]
+        The device to use for training.
+    """
+    model.train()
+
+    total_steps = len(data_loader)*epochs
+    progress_bar = tqdm(range(total_steps), desc="Training")
+
+    for epoch in range(epochs):
+        data_iter = iter(data_loader)
+        for x in data_iter:
+            x = x.to(device)
+            optimizer.zero_grad()
+            loss = model.loss(x)
+            loss.backward()
+            optimizer.step()
+
+            # Update progress bar
+            progress_bar.set_postfix(loss=f"⠀{loss.item():12.4f}", epoch=f"{epoch+1}/{epochs}")
+            progress_bar.update()
