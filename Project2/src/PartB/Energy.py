0c38f5c83087b92ba9ec89cbf50ada193bece973
Botong Yu <s214623@dtu.dk>
2026-03-28 16:14:00 +0100

    partB

---
+++ Project2/src/PartB/Energy.py
@@ -0,0 +1,27 @@
+import torch
+import torch.nn as nn
+import torch.distributions as td
+
+def curve_energy(curve, decoders):
+    """
+    curve: [N, M] tensor of points in latent space
+    decoders: nn.ModuleList of decoder models
+    """
+    N = curve.shape[0]
+    n_decoders = len(decoders)
+    energy = 0
+    
+    for i in range(N - 1):
+        # sample two random decoders l and k
+        l = torch.randint(0, n_decoders, (1,)).item()
+        k = torch.randint(0, n_decoders, (1,)).item()
+        
+        z_i = curve[i]
+        z_next = curve[i+1]
+        
+        f_l_zi = decoders[l](z_i.unsqueeze(0)).mean
+        f_k_znext = decoders[k](z_next.unsqueeze(0)).mean
+        
+        energy += torch.sum((f_l_zi - f_k_znext) ** 2)
+        
+    return energy
