fixed crucial order of batchnorm and softmax

Project2/src/helpers.py
@@ -14,11 +14,11 @@ def subsample(data, targets, num_data, num_classes):
 def new_encoder(M):
     encoder_net = nn.Sequential(
         nn.Conv2d(1, 16, 3, stride=2, padding=1),
-        nn.Softmax(),
         nn.BatchNorm2d(16),
+        nn.Softmax(dim=1),
         nn.Conv2d(16, 32, 3, stride=2, padding=1),
-        nn.Softmax(),
         nn.BatchNorm2d(32),
+        nn.Softmax(dim=1),
         nn.Conv2d(32, 32, 3, stride=2, padding=1),
         nn.Flatten(),
         nn.Linear(512, 2 * M),
@@ -29,14 +29,14 @@ def new_decoder(M):
     decoder_net = nn.Sequential(
         nn.Linear(M, 512),
         nn.Unflatten(-1, (32, 4, 4)),
-        nn.Softmax(),
         nn.BatchNorm2d(32),
+        nn.Softmax(dim=1),
         nn.ConvTranspose2d(32, 32, 3, stride=2, padding=1, output_padding=0),
-        nn.Softmax(),
         nn.BatchNorm2d(32),
+        nn.Softmax(dim=1),
         nn.ConvTranspose2d(32, 16, 3, stride=2, padding=1, output_padding=1),
-        nn.Softmax(),
         nn.BatchNorm2d(16),
+        nn.Softmax(dim=1),
         nn.ConvTranspose2d(16, 1, 3, stride=2, padding=1, output_padding=1),
     )
     return decoder_net