cd6bebf0576e6e862fe6d6fd835d2c27a35774a9
maltheaugust <s214631@dtu.dk>
2026-05-05 21:14:05 +0200

    refactor

--- Project3/train_network.py
+++ Project3/train_network.py
@@ -5,11 +5,15 @@ from torch_geometric.datasets import TUDataset
 from Project3.src.utils.config import SEED
 from Project3.src.utils.ELBO import ELBO
 
+from Project3.src.GraphVAE import GraphVAE
+
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 dataset = TUDataset(root='./data/', name='MUTAG').to(device)
 
 rng = torch.Generator().manual_seed(SEED)
 train_dataset, validation_dataset, test_dataset = random_split(dataset, (100, 44, 44), generator=rng)
+model = GraphVAE(in_channels=7, hidden_channels=32, latent_dim=16).to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
 
 
 def train():
